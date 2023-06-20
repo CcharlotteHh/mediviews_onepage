97ac7d5d37255fb376ee936967e1344af4ffd6d4
CcharlotteHh <210547@student.glu.nl>
2023-06-20 18:05:32 +0200

    uggh

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,37 +1,56 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from '../../pages/home/Home';
-import Contact from '../../pages/Contact';
-import styles from './nav.module.scss';
-import Logo from '../../assets/img/logo.png';
-import Hamburger from '../hamburger/Hamburger';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../../pages/home/Home";
+import { useState } from "react";
+import Contact from "../../pages/Contact";
+import styles from "./nav.module.scss";
+import Logo from "../../assets/img/logo.png";
+import Hamburger from "../hamburger/Hamburger";
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+    console.log("works");
+  }
   return (
     <Router>
-    <nav  className={styles.navbar}
->
-        <Link className={styles.logoLink} to="/" >
-        <img className={styles.logo}src={Logo} alt='logo'/>
-        </Link>
-        <ul className={styles.navLinks}>
-        <Link to="/contact" >
-        Contact
-        </Link>
-        <Link to="/instagram" >
-        Instagram
-        </Link>
-        </ul>
-        <Hamburger></Hamburger>
-    </nav>
+      <nav className={styles.nav}>
+        <div className={styles.navbar}>
 
-    
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>
-  )
+        
+
+          <Link className={styles.logoLink} to="/">
+            <img className={styles.logo} src={Logo} alt="logo" />
+          </Link>
+          <ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
+            <li className={styles.navLink}>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/instagram" className={styles.navLink}>
+                Instagram
+              </Link>
+            </li>
+            <li>
+              <Link to="/instagram" className={styles.navLink}>
+                Instagram
+              </Link>
+            </li>
+          </ul>
+          <div className="hamburger" onClick={toggleMenu}>
+            <Hamburger></Hamburger>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default Nav
+export default Nav;
